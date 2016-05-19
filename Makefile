82445c0b8895bf6eab41457f5d413bc07a333265
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-19 21:05:56 +0400

    + heap allocation/modify on REF object creation
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ wam.log: ./wam.exe
 	./wam.exe > $@ && tail $(TAIL) $@
 C = wam.cpp
 H = 
-CXXFLAGS += -std=gnu++11 -DHEAPsz=0x10
+CXXFLAGS += -std=gnu++11 -DHEAPsz=0x7
 ./wam.exe: $(C) $(H) Makefile
 	$(CXX) $(CXXFLAGS) -o $@ $(C)
--- wam.cpp
+++ wam.cpp
@@ -1,11 +1,44 @@
-struct CELL {
+// Prolog Virtual Machine
+// based on [hak] Hassan Aït-Kaci
+// Warren's Abstract Machine: A Tutorial Reconstruction
+
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+using namespace std;
+
+struct CELL {				// [hak,p.10] heap cell and HEAP array
+	string* tag;
 	int ref;
 } HEAP[HEAPsz];
+int HEAP_ALLOC = 0;
+
+struct WAM {				// basic class for heap cell
+	WAM();
+	static string tag;		// type tag
+	int hptr;				// pointer to cell in HEAP
+};
+string WAM::tag("(undef)");
+
+WAM::WAM() {
+	hptr = HEAP_ALLOC++; assert(HEAP_ALLOC < HEAPsz);	// alloc cell
+	HEAP[hptr].tag = &tag;								// mark tag
+	HEAP[hptr].ref = hptr;								// point to self
+}
+
+struct REF: WAM {
+	REF();
+	static string tag;
+};
+string REF::tag("REF");
+REF::REF() : WAM() { HEAP[hptr].tag = &tag; }
 
 void dump_heap() {
-	for (int i = 0; i < HEAPsz; i++)
-		printf("%.4X:\n", i);
+	for (int i = 0; i < HEAP_ALLOC; i++)
+		printf("%.4X:\t%s\t%i\n", i, HEAP[i].tag->c_str(), HEAP[i].ref);
 }
+
+REF X,Y,Z;
 
 int main() {
 	dump_heap();
